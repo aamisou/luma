2b00e924ad840f2c9b5340b302ede3316ff73222
aamisou <shijukv1979@gmail.com>
2026-03-22 16:41:17 +0530

    Add color palette definitions

---
+++ src/utils/colorPalette.ts
@@ -0,0 +1,12 @@
+// Color Palette Definitions
+
+const colorPalette = {
+    blushPink: '#F8C8DC',
+    lavender: '#D6C1FF',
+    babyBlue: '#BFE7FF',
+    mint: '#CFFFE5',
+    peach: '#FFD6A5',
+    cream: '#FFF8E7',
+};
+
+export default colorPalette;
