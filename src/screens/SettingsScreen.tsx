064998cb275a0de3a16ef9f83fe6b06a639db0af
aamisou <shijukv1979@gmail.com>
2026-03-22 16:43:33 +0530

    Create SettingsScreen placeholder

---
+++ src/screens/SettingsScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SettingsScreen: React.FC = () => {
+    return (
+        <div>
+            <h1>Settings Screen</h1>
+            {/* Additional settings options would go here */}
+        </div>
+    );
+};
+
+export default SettingsScreen;
