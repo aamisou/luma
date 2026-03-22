9a968ef2b91687b238646244be8dbe24d5c412a8
aamisou <shijukv1979@gmail.com>
2026-03-22 16:45:37 +0530

    Add FocusTimerScreen component

---
+++ src/screens/FocusTimerScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const FocusTimerScreen: React.FC = () => {
+    return (
+        <div>
+            <h1>Focus Timer Screen</h1>
+        </div>
+    );
+};
+export default FocusTimerScreen;
